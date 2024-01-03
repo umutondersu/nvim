f757db5c7bb9b573ef1d6ef4e8e0edbf95a8566c
Umut Önder <umutsonder@gmail.com>
2024-01-03 11:21:31 +0100

    barbar [buffer 1] fix and auto-session

--- lua/custom/plugins/barbar-nvim.lua
+++ lua/custom/plugins/barbar-nvim.lua
@@ -13,6 +13,7 @@ return {
         -- insert_at_start = true,
         -- …etc.
         clickable = true,
+        auto_hide = true,
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
 }
