57afe03762739c3cca112a78f27f9fe069dcc2de
Umut Önder <umutsonder@gmail.com>
2024-01-05 02:18:23 +0100

    improved nvim-tree compatibility

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -3,10 +3,31 @@ return {
     version = '*',
     lazy = false,
     dependencies = {
-        'nvim-tree/nvim-web-devicons',
+        {
+            'nvim-tree/nvim-web-devicons',
+            config = function()
+                require('nvim-web-devicons').setup {
+                    strict = true,
+                    default = true,
+                    override_by_filename = {
+                        ['package.json'] = {
+                            icon = '',
+                            color = '#326da8',
+                            name = 'json',
+                        },
+                    },
+                }
+            end,
+        },
     },
     keys = {
-        { '<leader><Tab>', function() require('nvim-tree.api').tree.change_root_to_node() end, desc = "Swap root to selected directory" }
+        {
+            '<leader><Tab>',
+            function()
+                require('nvim-tree.api').tree.change_root_to_node()
+            end,
+            desc = 'Swap root to selected directory',
+        },
     },
     config = function()
         require('nvim-tree').setup {
@@ -31,5 +52,33 @@ return {
                 end,
             },
         }
+        -- [[ Configure nvim-tree ]]
+        -- Disable netrw for nvim-tree and autoclose
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
+        -- auto close
+        vim.api.nvim_create_autocmd('QuitPre', {
+            callback = function()
+                local tree_wins = {}
+                local floating_wins = {}
+                local wins = vim.api.nvim_list_wins()
+                for _, w in ipairs(wins) do
+                    local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+                    if bufname:match 'NvimTree_' ~= nil then
+                        table.insert(tree_wins, w)
+                    end
+                    if vim.api.nvim_win_get_config(w).relative ~= '' then
+                        table.insert(floating_wins, w)
+                    end
+                end
+                if 1 == #wins - #floating_wins - #tree_wins then
+                    -- Should quit, so we close all invalid windows.
+                    for _, w in ipairs(tree_wins) do
+                        vim.api.nvim_win_close(w, true)
+                    end
+                end
+            end,
+        })
     end,
 }
