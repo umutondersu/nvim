24baff812940b580739ba1d8137fd6b0b4a6eba3
Umut Önder <umutsonder@gmail.com>
2024-01-06 13:41:02 +0100

    added new keybindings to git-sign

--- init.lua
+++ init.lua
@@ -184,7 +184,8 @@ require('lazy').setup({
         map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
         map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
         map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
+        map('n', '<leader>hp', gs.preview_hunk_inline, { desc = 'preview git hunk inline' })
+        map('n', '<leader>hP', gs.preview_hunk, { desc = 'preview git hunk' })
         map('n', '<leader>hb', function()
           gs.blame_line { full = false }
         end, { desc = 'git blame line' })
