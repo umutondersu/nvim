27516fa8c90d879f53dec7fa9b2667023d1d411f
Umut Önder <umutsonder@gmail.com>
2024-01-07 23:26:29 +0100

    configured lua lsp

--- init.lua
+++ init.lua
@@ -729,7 +729,7 @@ local servers = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
       -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = { 'missing-fields' } },
+      diagnostics = { disable = { 'missing-fields' } },
     },
   },
 }
@@ -805,7 +805,7 @@ cmp.setup {
       end
     end, { 'i', 's' }),
   },
-  formatting = { --ignore
+  formatting = {
     format = lspkind.cmp_format({
       -- mode = 'symbol', -- show only symbol annotations
       maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
