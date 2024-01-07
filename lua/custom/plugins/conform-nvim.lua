8bc56ea87a1a66ef13e6fc5bfc6ed65031bf9e38
Umut Önder <umutsonder@gmail.com>
2024-01-07 16:41:25 +0100

    updated conform-nvim to have the recommended config and added keybindings

--- lua/custom/plugins/conform-nvim.lua
+++ lua/custom/plugins/conform-nvim.lua
@@ -1,38 +1,92 @@
-return{
-    "stevearc/conform.nvim",
-    event = {
-        "BufReadPre",
-        "BufNewFile",
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+      end,
+      mode = '',
+      desc = '[F]ormatter options'
     },
-    config = function()
-        local conform = require("conform")
+    {
+      -- Customize or remove this keymap to your liking
+      '<leader>ff',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = 'Format buffer',
+    },
+    {
+      '<leader>fe',
+      function()
+        vim.cmd("Fenable")
+      end,
+      mode = '',
+      desc = "Re-enable autoformat-on-save",
+    },
+    {
+      '<leader>fd',
+      function()
+        vim.cmd("Fdisable")
+      end,
+      mode = '',
+      desc = "Disable autoformat-on-save",
+    }
+  },
+  -- Everything in opts will be passed to setup()
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      javascript = { { 'prettierd', 'prettier' } },
+      typescript = { { 'prettierd', 'prettier' } },
+      javascriptreact = { { 'prettierd', 'prettier' } },
+      typescriptreact = { { 'prettierd', 'prettier' } },
+      json = { { 'prettierd', 'prettier' } },
+      yaml = { { 'prettierd', 'prettier' } },
+      markdown = { { 'prettierd', 'prettier' } },
+      html = { { 'prettierd', 'prettier' } },
+      css = { { 'prettierd', 'prettier' } },
+      lua = { { 'stylua' } },
+      -- python = {"isort", "black"},
+    },
+    -- Customize formatters
+    -- formatters = {
+    --     shfmt = {
+    --         prepend_args = { '-i', '2' },
+    --     },
+    -- },
+  },
+  config = function()
+    require("conform").setup({
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
+    })
 
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { { "prettierd", "prettier" } },
-                typescript = { { "prettierd", "prettier" } },
-                javascriptreact = { { "prettierd", "prettier" } },
-                typescriptreact = { { "prettierd", "prettier" } },
-                json = { { "prettierd", "prettier" } },
-                yaml = { { "prettierd", "prettier" } },
-                markdown = { { "prettierd", "prettier" } },
-                html = { { "prettierd", "prettier" } },
-                css = { { "prettierd", "prettier" } },
-                lua = { { "stylua" } },
-                -- python = {"isort", "black"},
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            },
-        })
-        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-            conform.format({
-              lsp_fallback = true,
-              async = false,
-              timeout_ms = 500,
-            })
-          end, { desc = "Format file or range (in visual mode)" })
-    end,
+    vim.api.nvim_create_user_command("Fdisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("Fenable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+  end,
 }
+
