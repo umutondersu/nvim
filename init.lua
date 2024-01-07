c3338c302f7b96661a9d981428583fe3c1a8b648
Umut Önder <umutsonder@gmail.com>
2024-01-07 17:36:55 +0100

    lua-line customization

--- init.lua
+++ init.lua
@@ -218,15 +218,55 @@ require('lazy').setup({
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'auto',
-        component_separators = '|',
-        section_separators = '',
-        disabled_filetypes = { 'NvimTree' },
-      },
-    },
+    config = function()
+      -- Custom Lualine theme
+      local lualine_tokyonight = require('lualine.themes.tokyonight')
+      --change the color of section a background for all modes
+      local bgcolor = '#222638'
+      lualine_tokyonight.normal.b.bg = bgcolor
+      lualine_tokyonight.insert.b.bg = bgcolor
+      lualine_tokyonight.visual.b.bg = bgcolor
+      lualine_tokyonight.replace.b.bg = bgcolor
+      lualine_tokyonight.command.b.bg = bgcolor
+      lualine_tokyonight.inactive.b.bg = bgcolor
+      require 'lualine'.setup {
+        options = {
+          icons_enabled = true,
+          theme = lualine_tokyonight,
+          component_separators = { left = '', right = '|' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = { 'NvimTree' },
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype', 'copilot' },
+          lualine_y = {},
+          lualine_z = { 'location' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+      }
+    end,
   },
 
   {
