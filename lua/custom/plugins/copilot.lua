d20811a3127186cf285cc8c2b04e023fbb0e86c4
Umut Önder <umutsonder@gmail.com>
2024-01-07 17:38:35 +0100

    copilot integration

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,39 @@
+return {
+    {
+        'zbirenbaum/copilot.lua',
+        cmd = 'Copilot',
+        event = 'InsertEnter',
+        lazy = true,
+        config = function()
+            require('copilot').setup {
+                panel = {
+                    enabled = false,
+                },
+                suggestion = {
+                    enabled = false,
+                },
+                filetypes = {
+                    yaml = false,
+                    markdown = false,
+                    help = false,
+                    gitcommit = false,
+                    gitrebase = false,
+                    hgcommit = false,
+                    svn = false,
+                    cvs = false,
+                    ['.'] = false,
+                },
+                copilot_node_command = 'node',
+                server_opts_overrides = {},
+            }
+        end,
+    },
+    {
+        'zbirenbaum/copilot-cmp',
+        config = function()
+            require('copilot_cmp').setup()
+        end,
+    },
+    'AndreM222/copilot-lualine',
+}
+
