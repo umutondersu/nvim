1f1b3c75c5e281ada6e12b58794004f4ead885c1
Umut Önder <umutsonder@gmail.com>
2024-01-10 22:18:10 +0100

    merged formatting plugins

--- lua/kickstart/plugins/autoformat.lua
+++ lua/kickstart/plugins/autoformat.lua
@@ -4,71 +4,208 @@
 -- Adds additional commands as well to manage the behavior
 
 return {
-  'neovim/nvim-lspconfig',
-  config = function()
-    -- Switch for controlling whether you want autoformatting.
-    --  Use :KickstartFormatToggle to toggle autoformatting on or off
-    local format_is_enabled = true
-    vim.api.nvim_create_user_command('KickstartFormatToggle', function()
-      format_is_enabled = not format_is_enabled
-      print('Setting autoformatting to: ' .. tostring(format_is_enabled))
-    end, {})
+  {
+    'neovim/nvim-lspconfig',
+    keys = {
+      {
+        '<leader>f',
+        function()
+        end,
+        mode = '',
+        desc = '[F]ormatter options'
+      },
+      {
+        '<leader>fl',
+        function()
+        end,
+        mode = '',
+        desc = 'LSP Formatting'
+      },
+      {
+        '<leader>flf',
+        function()
+          vim.lsp.buf.format {
+            async = false,
+          }
+        end,
+        mode = '',
+        desc = 'Format buffer with LSP',
+      },
+      {
+        '<leader>flt',
+        function()
+          vim.cmd("KickstartFormatToggle")
+        end,
+        mode = '',
+        desc = "Toggle autoformatting for LSP",
+      },
+    },
+    config = function()
+      -- Switch for controlling whether you want autoformatting.
+      --  Use :KickstartFormatToggle to toggle autoformatting on or off
+      local format_is_enabled = false
+      vim.api.nvim_create_user_command('KickstartFormatToggle', function()
+        format_is_enabled = not format_is_enabled
+        print('Setting autoformatting to: ' .. tostring(format_is_enabled))
+      end, {})
 
-    -- Create an augroup that is used for managing our formatting autocmds.
-    --      We need one augroup per client to make sure that multiple clients
-    --      can attach to the same buffer without interfering with each other.
-    local _augroups = {}
-    local get_augroup = function(client)
-      if not _augroups[client.id] then
-        local group_name = 'kickstart-lsp-format-' .. client.name
-        local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-        _augroups[client.id] = id
+      -- Create an augroup that is used for managing our formatting autocmds.
+      --      We need one augroup per client to make sure that multiple clients
+      --      can attach to the same buffer without interfering with each other.
+      local _augroups = {}
+      local get_augroup = function(client)
+        if not _augroups[client.id] then
+          local group_name = 'kickstart-lsp-format-' .. client.name
+          local id = vim.api.nvim_create_augroup(group_name, { clear = true })
+          _augroups[client.id] = id
+        end
+
+        return _augroups[client.id]
       end
 
-      return _augroups[client.id]
-    end
+      -- Whenever an LSP attaches to a buffer, we will run this function.
+      --
+      -- See `:help LspAttach` for more information about this autocmd event.
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
+        -- This is where we attach the autoformatting for reasonable clients
+        callback = function(args)
+          local client_id = args.data.client_id
+          local client = vim.lsp.get_client_by_id(client_id)
+          local bufnr = args.buf
 
-    -- Whenever an LSP attaches to a buffer, we will run this function.
-    --
-    -- See `:help LspAttach` for more information about this autocmd event.
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
-      -- This is where we attach the autoformatting for reasonable clients
-      callback = function(args)
-        local client_id = args.data.client_id
-        local client = vim.lsp.get_client_by_id(client_id)
-        local bufnr = args.buf
+          -- Only attach to clients that support document formatting
+          if not client.server_capabilities.documentFormattingProvider then
+            return
+          end
 
-        -- Only attach to clients that support document formatting
-        if not client.server_capabilities.documentFormattingProvider then
+          -- Tsserver usually works poorly. Sorry you work with bad languages
+          -- You can remove this line if you know what you're doing :)
+          if client.name == 'tsserver' then
+            return
+          end
+
+          -- Create an autocmd that will run *before* we save the buffer.
+          --  Run the formatting command for the LSP that has just attached.
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = get_augroup(client),
+            buffer = bufnr,
+            callback = function()
+              if not format_is_enabled then
+                return
+              end
+
+              vim.lsp.buf.format {
+                async = false,
+                filter = function(c)
+                  return c.id == client.id
+                end,
+              }
+            end,
+          })
+        end,
+      })
+    end,
+  }, {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+      end,
+      mode = '',
+      desc = '[F]ormatter options'
+    },
+    {
+      '<leader>ff',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = 'Format buffer',
+    },
+    {
+      '<leader>ft',
+      function()
+        vim.cmd("Ftoggle")
+      end,
+      mode = '',
+      desc = 'Toggle autoformatting',
+    },
+    -- {
+    --     '<leader>fe',
+    --     function()
+    --         vim.cmd("Fenable")
+    --     end,
+    --     mode = '',
+    --     desc = "Re-enable autoformat-on-save",
+    -- },
+    -- {
+    --     '<leader>fd',
+    --     function()
+    --         vim.cmd("Fdisable")
+    --     end,
+    --     mode = '',
+    --     desc = "Disable autoformat-on-save",
+    -- }
+  },
+  -- Everything in opts will be passed to setup()
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      javascript = { { 'prettierd', 'prettier' } },
+      typescript = { { 'prettierd', 'prettier' } },
+      javascriptreact = { { 'prettierd', 'prettier' } },
+      typescriptreact = { { 'prettierd', 'prettier' } },
+      json = { { 'prettierd', 'prettier' } },
+      yaml = { { 'prettierd', 'prettier' } },
+      markdown = { { 'prettierd', 'prettier' } },
+      html = { { 'prettierd', 'prettier' } },
+      css = { { 'prettierd', 'prettier' } },
+      lua = { { 'stylua' } },
+      -- python = {"isort", "black"},
+    },
+    -- Customize formatters
+    -- formatters = {
+    --     shfmt = {
+    --         prepend_args = { '-i', '2' },
+    --     },
+    -- },
+  },
+  config = function()
+    require("conform").setup({
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
           return
         end
-
-        -- Tsserver usually works poorly. Sorry you work with bad languages
-        -- You can remove this line if you know what you're doing :)
-        if client.name == 'tsserver' then
-          return
-        end
-
-        -- Create an autocmd that will run *before* we save the buffer.
-        --  Run the formatting command for the LSP that has just attached.
-        vim.api.nvim_create_autocmd('BufWritePre', {
-          group = get_augroup(client),
-          buffer = bufnr,
-          callback = function()
-            if not format_is_enabled then
-              return
-            end
-
-            vim.lsp.buf.format {
-              async = false,
-              filter = function(c)
-                return c.id == client.id
-              end,
-            }
-          end,
-        })
+        return { timeout_ms = 500, lsp_fallback = true }
       end,
     })
+
+    -- vim.api.nvim_create_user_command("Fdisable", function(args)
+    --     if args.bang then
+    --         -- FormatDisable! will disable formatting just for this buffer
+    --         vim.b.disable_autoformat = true
+    --     else
+    --         vim.g.disable_autoformat = true
+    --     end
+    -- end, {
+    --     desc = "Disable autoformat-on-save",
+    --     bang = true,
+    -- })
+    -- vim.api.nvim_create_user_command("Fenable", function()
+    --     vim.b.disable_autoformat = false
+    --     vim.g.disable_autoformat = false
+    -- end, {
+    --     desc = "Re-enable autoformat-on-save",
+    -- })
+    vim.api.nvim_create_user_command("Ftoggle", function()
+      vim.b.disable_autoformat = not vim.b.disable_autoformat
+      vim.g.disable_autoformat = not vim.g.disable_autoformat
+      print('Setting autoformatting to: ' .. tostring(not vim.g.disable_autoformat))
+    end, { desc = "Toggle autoformatting" })
   end,
-}
+} }
