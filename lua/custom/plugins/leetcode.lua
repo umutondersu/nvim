13402b6425ecbb1e37d59396d682a251421c443e
Umut Önder <umutsonder@gmail.com>
2024-02-02 00:53:52 +0100

    added keymaps to leetcode.nvim

--- lua/custom/plugins/leetcode.lua
+++ lua/custom/plugins/leetcode.lua
@@ -18,5 +18,52 @@ return {
         lang = 'python3',
         -- directory = os.getenv('HOME') .. '/leetcode',
     },
+    keys = { {
+        '<leader>l',
+        function()
+        end,
+        desc = '+[L]eetcode',
+        ft = { 'leetcode.nvim' }
+    }, {
+        '<leader>lc',
+        '<cmd> Leet console<cr>',
+        desc = 'open [C]onsole popup',
+        ft = { 'leetcode.nvim' }
+    }, {
+        '<leader>li',
+        '<cmd> Leet info<cr>',
+        desc = 'open pop-up containing [I]nfo',
+        ft = { 'leetcode.nvim' }
+    }, {
+        '<leader>ly',
+        '<cmd> Leet yank<cr>',
+        desc = '[Y]ank current solution',
+        ft = { 'leetcode.nvim' }
+    }, {
+        '<leader>ll',
+        '<cmd> Leet lang<cr>',
+        desc = 'change [L]anguage',
+        ft = { 'leetcode.nvim' }
+    }, {
+        '<leader>lr',
+        '<cmd> Leet run<cr>',
+        desc = '[R]un current solution',
+        ft = { 'leetcode.nvim' }
+    }, {
+        '<leader>lt',
+        '<cmd> Leet test<cr>',
+        desc = '[T]est current solution',
+        ft = { 'leetcode.nvim' }
+    }, {
+        '<leader>ls',
+        '<cmd> Leet submit<cr>',
+        desc = '[S]ubmit current solution',
+        ft = { 'leetcode.nvim' }
+    }, {
+        '<leader>ld',
+        '<cmd> Leet desc<cr>',
+        desc = 'toggle question [D]escription',
+        ft = { 'leetcode.nvim' }
+    },
+    },
 }
-
