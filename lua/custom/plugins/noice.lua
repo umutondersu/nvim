7671f900e3806dcd23d4cd2b07017c113e7073fa
Umut Önder <umutsonder@gmail.com>
2024-02-05 15:42:10 +0100

    noice improvements

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -4,12 +4,14 @@ return {
     enabled = true,
     opts = {
         cmdline = { view = "cmdline" },
+        messages = { view = false },
         popupmenu = { enabled = false },
         lsp = {
             -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
             override = {
                 ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                 ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = false, -- requires hrsh7th/nvim-cmp
             },
             progress = { enabled = false },
         },
@@ -23,5 +25,9 @@ return {
     dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify"
     }
 }
