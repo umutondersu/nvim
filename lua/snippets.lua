4347b56674f9e63ee418aa340bc0af65271dea4e
Umut Önder <umutsonder@gmail.com>
2024-03-02 00:30:14 +0100

    Added custom go snippets

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -10,6 +10,8 @@ local all_js_file_types = {
   "typescriptreact",
 }
 
+-- All JS file types
+-- console.log
 for _, filetype in pairs(all_js_file_types) do
   ls.add_snippets(filetype, {
     s({ trig = "clg", desc = "Output a message to the console" }, { t('console.log('), i(1), t(')') }),
@@ -17,3 +19,21 @@ for _, filetype in pairs(all_js_file_types) do
   )
 end
 
+-- GO
+-- nil err check
+ls.add_snippets("go", {
+  s({ trig = "errn", desc = "Nil Error check" }, {
+    t("if err != nil {"),
+    t({ "", "\t" }), i(1),
+    t({ "", "}" }),
+  }),
+})
+
+
+ls.add_snippets("go", {
+  s({ trig = "errif", desc = "If Error check" }, {
+    t("if err == "), i(1), t("{"),
+    t({ "", "\t" }), i(2),
+    t({ "", "}" }),
+  }),
+})
