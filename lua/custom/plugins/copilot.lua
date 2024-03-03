8508b636c84ce31c02075b46b6c4d76f2d54f680
Umut Önder <umutsonder@gmail.com>
2024-03-03 21:19:37 +0100

    replaced copilot chat extension

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -32,31 +32,46 @@ return {
         opts = {},
     },
     {
-        "jellydn/CopilotChat.nvim",
-        event = "VeryLazy",
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
         enabled = vim.fn.has("win32") == 0,
+        event = "VeryLazy",
+        keys = {
+            { "<leader>cb", ":CopilotChatBuffer ",               desc = "CopilotChat - Chat with current buffer" },
+            { "<leader>ce", "<cmd>CopilotChatExplain<cr>",       desc = "CopilotChat - Explain code [Prompt]" },
+            { "<leader>ct", "<cmd>CopilotChatTests<cr>",         desc = "CopilotChat - Generate tests [Prompt]" },
+            { "<leader>cv", "<cmd>CopilotChatVsplitToggle<cr>",  desc = "CopilotChat - Toggle Chat Window",      mode = "n" },
+            { "<leader>cv", ":CopilotChatVisual ",               desc = "CopilotChat - Open in vertical split",  mode = "x" },
+            { "<leader>cx", ":CopilotChatInPlace<cr>",           desc = "CopilotChat - Run in-place code",       mode = "x" },
+            { "<leader>cf", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "CopilotChat - Fix diagnostic" },
+            { "<leader>cR", "<cmd>CopilotChatReset<cr>",         desc = "CopilotChat - Reset Chat Buffer" },
+            { "<leader>cr", "<cmd>CopilotChatReview<cr>",        desc = "CopilotChat - Review code [Prompt]" },
+            { "<leader>cE", "<cmd>CopilotChatRefactor<cr>",      desc = "CopilotChat - Refactor code [Prompt]" },
+            {
+                "<leader>ch",
+                function() require("CopilotChat.code_actions").show_help_actions() end,
+                desc = "CopilotChat - Help actions",
+            },
+            -- {
+            --     "<leader>cp",
+            --     function() require("CopilotChat.code_actions").show_prompt_actions() end,
+            --     desc = "CopilotChat - Prompt actions",
+            -- },
+        },
         opts = {
-            mode = "split", -- newbuffer or split  , default: newbuffer
+            show_help = "yes",         -- Show help text for CopilotChatInPlace, default: yes
+            debug = false,             -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+            disable_extra_info = 'no', -- Disable extra information (e.g: system prompt) in the response.
+            language = "English",
             prompts = {
                 Explain = "Explain how it works.",
                 Review = "Review the following code and provide concise suggestions.",
                 Tests = "Briefly explain how the selected code works, then generate unit tests.",
                 Refactor = "Refactor the code to improve clarity and readability.",
-                Summarize = "Please summarize the following text.",
-            },
+            }
         },
         build = function()
-            vim.defer_fn(function()
-                vim.cmd("UpdateRemotePlugins")
-                vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim.")
-            end, 3000)
+            vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
         end,
-        keys = {
-            { "<leader>Ce", "<cmd>CopilotChatExplain<cr>",   desc = "Explain code" },
-            { "<leader>Ct", "<cmd>CopilotChatTests<cr>",     desc = "Generate tests" },
-            { "<leader>Cr", "<cmd>CopilotChatReview<cr>",    desc = "Review code" },
-            { "<leader>CR", "<cmd>CopilotChatRefactor<cr>",  desc = "Refactor code" },
-            { "<leader>Cs", "<cmd>CopilotChatSummarize<cr>", desc = "Summarize text" },
-        },
     },
 }
