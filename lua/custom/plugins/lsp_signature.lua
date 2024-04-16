b453c016f8a7522339c9bece89f7924531692ad9
Umut Önder <umutsonder@gmail.com>
2024-04-16 15:03:45 +0200

    disabled floating window in lsp_signature

--- lua/custom/plugins/lsp_signature.lua
+++ lua/custom/plugins/lsp_signature.lua
@@ -1,6 +1,8 @@
 return {
     "ray-x/lsp_signature.nvim",
     event = "VeryLazy",
-    opts = { hint_prefix = " ", },
-    config = function(_, opts) require "lsp_signature".setup(opts) end
+    opts = {
+        hint_prefix = " ",
+        floating_window = false,
+    },
 }
