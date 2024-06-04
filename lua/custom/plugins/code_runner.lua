6e6aa9ad17456c4f229af4e253a4a91ff5a564b9
Umut Önder <umutsonder@gmail.com>
2024-06-05 01:46:56 +0200

    Improved code_runner config

--- lua/custom/plugins/code_runner.lua
+++ lua/custom/plugins/code_runner.lua
@@ -2,11 +2,11 @@ return {
     "CRAG666/code_runner.nvim",
     config = function()
         local function jsruntime()
-            if vim.fn.has('win32') == 1
+            if require("lib").command_exists('bun')
             then
-                return "node run"
-            else
                 return "bun run"
+            else
+                return "node run"
             end
         end
         require('code_runner').setup({
@@ -23,8 +23,23 @@ return {
                     "rustc $fileName &&",
                     "$dir/$fileNameWithoutExt"
                 },
+                c = function(...)
+                    local c_base = {
+                        "cd $dir &&",
+                        "gcc $fileName -o",
+                        "/tmp/$fileNameWithoutExt",
+                    }
+                    local c_exec = {
+                        "&& /tmp/$fileNameWithoutExt &&",
+                        "rm /tmp/$fileNameWithoutExt",
+                    }
+                    vim.ui.input({ prompt = "Add more args:" }, function(input)
+                        c_base[4] = input
+                        vim.print(vim.tbl_extend("force", c_base, c_exec))
+                        require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
+                    end)
+                end,
             },
         })
     end
 }
-
