71f781ce069d964eb06a69377c3b4e9c232177f8
Umut Sahin Onder <umutsonder@gmail.com>
2024-06-27 22:14:00 +0200

    Better Syntax for transparent and non-transparent colorschemes choosing

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -5,34 +5,43 @@ return {
     priority = 1000,
     opts = {},
     config = function()
-      -- Transparent background
-      -- require("tokyonight").setup({
-      --   transparent = true,
-      --   styles = {
-      --     sidebars = "transparent",
-      --     floats = "transparent",
-      --
-      --   },
-      --   on_highlights = function(hl)
-      --     local color = "#898da0"
-      --     hl.LineNr = {
-      --       fg = color,
-      --     }
-      --     hl.LineNrAbove = {
-      --       fg = color,
-      --     }
-      --     hl.LineNrBelow = {
-      --       fg = color,
-      --     }
-      --   end,
-      -- })
-      require("tokyonight").setup {}
-      vim.api.nvim_create_autocmd('ColorScheme', {
-        callback = vim.schedule_wrap(function()
-          vim.cmd('hi BufferTabpageFill guibg=none')
-        end),
-        group = vim.api.nvim_create_augroup('foo', {}),
-      })
+      local Transparent = false
+      if Transparent then
+        require("tokyonight").setup({
+          transparent = true,
+          styles = {
+            sidebars = "transparent",
+            floats = "transparent",
+          },
+          on_highlights = function(hl)
+            local LineNrHighlight = "#898da0"
+            hl.LineNr = {
+              fg = LineNrHighlight,
+            }
+            hl.LineNrAbove = {
+              fg = LineNrHighlight,
+            }
+            hl.LineNrBelow = {
+              fg = LineNrHighlight,
+            }
+          end,
+        })
+        vim.api.nvim_create_autocmd('ColorScheme', {
+          callback = vim.schedule_wrap(function()
+            vim.cmd('hi BufferTabpageFill guibg=none')
+            vim.cmd('hi BufferOffset guibg=none')
+          end),
+          group = vim.api.nvim_create_augroup('foo', {}),
+        })
+      else
+        require("tokyonight").setup()
+        vim.api.nvim_create_autocmd('ColorScheme', {
+          callback = vim.schedule_wrap(function()
+            vim.cmd('hi BufferTabpageFill guibg=none')
+          end),
+          group = vim.api.nvim_create_augroup('foo', {}),
+        })
+      end
       vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
