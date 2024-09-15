7cb4a47c8cb60c0dddaa553bc0a86165230d30b0
Umut Sahin Onder <umutsonder@gmail.com>
2024-09-15 21:35:55 +0200

    Updated which-key hints for virtual mode

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -6,12 +6,12 @@
 --   opts = {
 --     preset = "modern",
 --     spec = {
---       { "<leader>c",  group = "[C]opilot Chat" },
---       { "<leader>f",  group = "[F]ormat" },
+--       { "<leader>c",  group = "[C]opilot Chat",       mode = { 'n', 'v' } },
+--       { "<leader>f",  group = "[F]ormat",       mode = { 'n', 'v' } },
 --       { "<leader>g",  group = "[G]it" },
 --       { "<leader>h",  group = "Git [H]unk",       mode = { 'n', 'v' } },
 --       { "<leader>n",  group = "[N]o Neck Pain" },
---       { "<leader>r",  group = "[R]ename" },
+--       { "<leader>r",  group = "[R]ename" ,       mode = { 'n', 'v' }},
 --       { "<leader>s",  group = "[S]earch" },
 --       { "<leader>sn", group = "[S]earch [N]eovim" },
 --       { "<leader>t",  group = "[T]est" },
@@ -26,12 +26,12 @@ return {
         require("which-key").setup()
         require('which-key').register {
             ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-            ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-            ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+            ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore', mode = { 'n', 'v' } },
+            ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore', mode = { 'n', 'v' } },
             ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
             ['<leader>sn'] = { name = '[S]earch [N]eovim', _ = 'which_key_ignore' },
-            ['<leader>f'] = { name = '[F]ormat', _ = 'which_key_ignore' },
-            ['<leader>c'] = { name = '[C]opilot Chat', _ = 'which_key_ignore' },
+            ['<leader>f'] = { name = '[F]ormat', _ = 'which_key_ignore', mode = { 'n', 'v' } },
+            ['<leader>c'] = { name = '[C]opilot Chat', _ = 'which_key_ignore', mode = { 'n', 'v' } },
             ['<leader>n'] = { name = '[N]o Neck Pain', _ = 'which_key_ignore' },
             ['<leader>t'] = { name = '[T]est', _ = 'which_key_ignore' },
             ['gp'] = { name = '[P]review', _ = 'which_key_ignore' },
