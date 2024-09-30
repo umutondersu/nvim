eaa1b95edf786e2716bdc0823ac242c18d6273e2
Umut Sahin Onder <umutsonder@gmail.com>
2024-09-30 18:35:03 +0200

    Fixed dropbar plugin keymap crashing

--- lua/custom/plugins/dropbar.lua
+++ lua/custom/plugins/dropbar.lua
@@ -1,16 +1,12 @@
 -- TODO: Needs to be integerated with barbar.nvim as it makes the Winbar clutered
 return {
     'Bekaboo/dropbar.nvim',
-    -- optional, but required for fuzzy finder support
     dependencies = {
         'nvim-telescope/telescope-fzf-native.nvim',
         'nvim-web-devicons'
     },
-    keys = {
-        {
-            "<leader>p",
-            function() require('dropbar.api').pick() end,
-            desc = "[P]ick Mode for Dropbar",
-        },
-    },
+    config = function()
+        vim.keymap.set('n', '<leader>p', "<cmd>lua require('dropbar.api').pick()<cr>",
+            { desc = '[P]ick mode for dropbar' })
+    end
 }
