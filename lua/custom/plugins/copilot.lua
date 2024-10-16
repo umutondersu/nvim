02a6dad9071afb607dd64c4b889e76b53d60c008
Umut Sahin Onder <umutsonder@gmail.com>
2024-10-17 02:13:55 +0300

    refactored copilot config

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,29 +3,27 @@ return {
         'zbirenbaum/copilot.lua',
         cmd = 'Copilot',
         event = 'InsertEnter',
-        config = function()
-            require('copilot').setup {
-                panel = {
-                    enabled = false,
-                },
-                suggestion = {
-                    enabled = false,
-                },
-                filetypes = {
-                    yaml = false,
-                    markdown = false,
-                    help = false,
-                    gitcommit = false,
-                    gitrebase = false,
-                    hgcommit = false,
-                    svn = false,
-                    cvs = false,
-                    ['.'] = false,
-                },
-                copilot_node_command = 'node',
-                server_opts_overrides = {},
-            }
-        end,
+        opts = {
+            panel = {
+                enabled = false,
+            },
+            suggestion = {
+                enabled = false,
+            },
+            filetypes = {
+                yaml = false,
+                markdown = false,
+                help = false,
+                gitcommit = false,
+                gitrebase = false,
+                hgcommit = false,
+                svn = false,
+                cvs = false,
+                ['.'] = false,
+            },
+            copilot_node_command = 'node',
+            server_opts_overrides = {},
+        }
     },
     {
         'zbirenbaum/copilot-cmp',
