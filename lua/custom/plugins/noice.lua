14b8b63cb42241ccbe55bce1112997d59e3a1839
Umut Sahin Onder <umutsonder@gmail.com>
2024-12-21 01:43:00 +0100

    disabled all signature helps but noice

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -13,7 +13,6 @@ return {
                 ["vim.lsp.util.stylize_markdown"] = true,
                 ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
             },
-            signature = { enabled = false },
         },
         -- you can enable a preset for easier configuration
         presets = {
