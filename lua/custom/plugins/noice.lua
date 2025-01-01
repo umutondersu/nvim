91b4bc6ea3fd3aa1ca14376cb786c9f883167b75
Umut Sahin Onder <umutsonder@gmail.com>
2025-01-01 19:38:09 +0300

    Removed unncessary settings from noice config

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,11 +1,8 @@
 return {
     "folke/noice.nvim",
     event = "VeryLazy",
-    enabled = true,
     opts = {
         cmdline = { view = "cmdline" },
-        popupmenu = { enabled = false },
-        notify = { view = "notify" },
         lsp = {
             -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
             override = {
@@ -16,8 +13,6 @@ return {
         },
         -- you can enable a preset for easier configuration
         presets = {
-            bottom_search = true,          -- use a classic bottom cmdline for search
-            command_palette = true,        -- position the cmdline and popupmenu together
             long_message_to_split = false, -- long messages will be sent to a split
             lsp_doc_border = true,         -- add a border to hover docs and signature help
         },
