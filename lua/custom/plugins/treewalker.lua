5946707521ea6b84293b0caffb9664ba41fcc16e
Umut Sahin Onder <umutsonder@gmail.com>
2025-01-08 03:16:40 +0100

    Minor desc fix in treewalker

--- lua/custom/plugins/treewalker.lua
+++ lua/custom/plugins/treewalker.lua
@@ -3,8 +3,8 @@ return {
     keys = {
         { '{',     '<cmd>Treewalker Up<cr>',        mode = { 'n', 'v' }, silent = true },
         { '}',     '<cmd>Treewalker Down<cr>',      mode = { 'n', 'v' }, silent = true },
-        { '[g',    '<cmd>Treewalker Right<cr>',     mode = { 'n', 'v' }, silent = true, desc = 'Go Treewalker Right' },
-        { ']g',    '<cmd>Treewalker Left<cr>',      mode = { 'n', 'v' }, silent = true, desc = 'Go Treewalker Left' },
+        { '[g',    '<cmd>Treewalker Right<cr>',     mode = { 'n', 'v' }, silent = true, desc = 'Go Right with Treewalker' },
+        { ']g',    '<cmd>Treewalker Left<cr>',      mode = { 'n', 'v' }, silent = true, desc = 'Go Left with Treewalker' },
 
         { '<C-{>', '<cmd>Treewalker SwapDown<cr>',  mode = 'n',          silent = true },
         { '<C-}>', '<cmd>Treewalker SwapUp<cr>',    mode = 'n',          silent = true },
