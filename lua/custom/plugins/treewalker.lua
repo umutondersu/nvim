31d24662f06dd17005e881934368dd4347907358
Umut Sahin Onder <umutsonder@gmail.com>
2025-01-09 04:15:50 +0100

    Fixed treewalker keymaps

--- lua/custom/plugins/treewalker.lua
+++ lua/custom/plugins/treewalker.lua
@@ -3,11 +3,11 @@ return {
     keys = {
         { '{',     '<cmd>Treewalker Up<cr>',        mode = { 'n', 'v' }, silent = true },
         { '}',     '<cmd>Treewalker Down<cr>',      mode = { 'n', 'v' }, silent = true },
-        { '[g',    '<cmd>Treewalker Right<cr>',     mode = { 'n', 'v' }, silent = true, desc = 'Go Right with Treewalker' },
-        { ']g',    '<cmd>Treewalker Left<cr>',      mode = { 'n', 'v' }, silent = true, desc = 'Go Left with Treewalker' },
+        { ']g',    '<cmd>Treewalker Right<cr>',     mode = { 'n', 'v' }, silent = true, desc = 'Go Right with Treewalker' },
+        { '[g',    '<cmd>Treewalker Left<cr>',      mode = { 'n', 'v' }, silent = true, desc = 'Go Left with Treewalker' },
 
-        { '<C-{>', '<cmd>Treewalker SwapDown<cr>',  mode = 'n',          silent = true },
-        { '<C-}>', '<cmd>Treewalker SwapUp<cr>',    mode = 'n',          silent = true },
+        { '<C-{>', '<cmd>Treewalker SwapUp<cr>',    mode = 'n',          silent = true },
+        { '<C-}>', '<cmd>Treewalker SwapDown<cr>',  mode = 'n',          silent = true },
         { '<C-]>', '<cmd>Treewalker SwapRight<CR>', mode = 'n',          silent = true },
         { '<C-[>', '<cmd>Treewalker SwapLeft<CR>',  mode = 'n',          silent = true },
     }
