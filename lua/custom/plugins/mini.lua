a7d9a7560edd9270fa1f00d30eb6212f58d4bda5
Umut Sahin Onder <umutsonder@gmail.com>
2025-01-20 19:30:12 +0100

    mini.surround update

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -14,8 +14,20 @@ return { -- Collection of various small independent plugins/modules
         --
         -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
         -- - sd'   - [S]urround [D]elete [']quotes
-        -- - sr)'  - [S]urround [R]eplace [)] [']
-        require('mini.surround').setup()
+        -- - sc)'  - [S]urround [C]hange [)] [']
+        require('mini.surround').setup({
+            mappings = {
+                add = 'sa',            -- Add surrounding in Normal and Visual modes
+                delete = 'sd',         -- Delete surrounding
+                find = 'sf',           -- Find surrounding (to the right)
+                find_left = 'sF',      -- Find surrounding (to the left)
+                highlight = 'sh',      -- Highlight surrounding
+                replace = 'sc',        -- Replace surrounding
+                update_n_lines = 'sn', -- Update `n_lines`
+                suffix_last = 'l',     -- Suffix to search with "prev" method
+                suffix_next = 'n',     -- Suffix to search with "next" method
+            },
+        })
 
         -- Extras
         require('mini.pairs').setup()
