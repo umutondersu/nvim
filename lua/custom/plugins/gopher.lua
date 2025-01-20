06d71e39a5d0e13227c434b76339ab5b3397badc
Umut Sahin Onder <umutsonder@gmail.com>
2025-01-20 13:20:53 +0100

    updated gopher config

--- lua/custom/plugins/gopher.lua
+++ lua/custom/plugins/gopher.lua
@@ -6,5 +6,16 @@ return {
         "nvim-treesitter/nvim-treesitter",
         "mfussenegger/nvim-dap", -- (optional) only if you use `gopher.dap`
     },
-    opts = {},
+    config = function()
+        local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { desc = 'LSP: ' .. desc })
+        end
+        local gopher = require("gopher")
+        gopher.setup({})
+        map('<leader>lt', gopher.tags.add, 'Add JSON tags to struct fields')
+        map('<leader>ld', '<cmd>GoCmt<cr>', 'Generate boilerplate for doc comments')
+        map('<leader>tgf', gopher.test.add, 'Generate Test for the function under the cursor')
+        map('<leader>tgF', gopher.test.all, 'Generate Test for all the functions in the file')
+    end
 }
