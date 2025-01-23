4b319d623be5c9e79a59aee4f5bca813a2ed600e
Umut Sahin Onder <umutsonder@gmail.com>
2025-01-23 21:36:11 +0100

    updatef flash.nvim keymaps

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -4,7 +4,7 @@ return {
     event = "VeryLazy",
     opts = {},
     keys = {
-        { "<M-s>", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash Jump" },
-        { "<M-S>", mode = { "n", "o" },      function() require("flash").treesitter() end, desc = "Flash Treesitter Selection" },
+        { "S",     mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash Jump" },
+        { "<M-s>", mode = { "n", "o" },      function() require("flash").treesitter() end, desc = "Flash Treesitter Selection" },
     },
 }
