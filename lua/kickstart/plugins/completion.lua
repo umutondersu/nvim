c7d2e171a17638074a3e7a3f1bc03a4c3321af8d
Umut Sahin Onder <umutsonder@gmail.com>
2025-01-29 02:55:42 +0100

    replaced blink-cmp-copilot with blink-copilot for better performance

--- lua/kickstart/plugins/completion.lua
+++ lua/kickstart/plugins/completion.lua
@@ -6,7 +6,7 @@ return {
 
     -- Sources
     'kristijanhusak/vim-dadbod-completion',
-    'giuxtaposition/blink-cmp-copilot',
+    "fang2hou/blink-copilot",
     'folke/lazydev.nvim',
 
     -- Snippet Engine
@@ -67,17 +67,13 @@ return {
       providers = {
         copilot = {
           name = "copilot",
-          module = "blink-cmp-copilot",
+          module = "blink-copilot",
+          score_offset = 100,
           async = true,
-          transform_items = function(_, items)
-            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-            local kind_idx = #CompletionItemKind + 1
-            CompletionItemKind[kind_idx] = "Copilot"
-            for _, item in ipairs(items) do
-              item.kind = kind_idx
-            end
-            return items
-          end,
+          opts = {
+            max_completions = 3,
+            max_attempts = 4,
+          }
         },
         lazydev = {
           name = "LazyDev",
@@ -98,36 +94,6 @@ return {
       nerd_font_variant = 'mono',
       kind_icons = {
         Copilot = "",
-        Text = '󰉿',
-        Method = '󰊕',
-        Function = '󰊕',
-        Constructor = '󰒓',
-
-        Field = '󰜢',
-        Variable = '󰆦',
-        Property = '󰖷',
-
-        Class = '󱡠',
-        Interface = '󱡠',
-        Struct = '󱡠',
-        Module = '󰅩',
-
-        Unit = '󰪚',
-        Value = '󰦨',
-        Enum = '󰦨',
-        EnumMember = '󰦨',
-
-        Keyword = '󰻾',
-        Constant = '󰏿',
-
-        Snippet = '󱄽',
-        Color = '󰏘',
-        File = '󰈔',
-        Reference = '󰬲',
-        Folder = '󰉋',
-        Event = '󱐋',
-        Operator = '󰪚',
-        TypeParameter = '󰬛',
       },
     },
 
