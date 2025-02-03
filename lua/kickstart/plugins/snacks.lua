898a04f64cb4914bb0d92a542e54bc50b07aa36f
Umut Sahin Onder <umutsonder@gmail.com>
2025-02-03 17:48:45 +0100

    updated snacks config

--- lua/kickstart/plugins/snacks.lua
+++ lua/kickstart/plugins/snacks.lua
@@ -18,51 +18,94 @@ return {
             }
         },
         scope = { enabled = true }, -- Jumps: ]i [i textobjects: ii(inner scope) ai(full scope)
+        picker = {
+            matcher = {
+                frecency = true,
+            },
+        },
     },
     keys = {
-        { "<leader>St",  function() Snacks.scratch() end,                 desc = "Toggle Scratch Buffer" },
-        { "<leader>Ss",  function() Snacks.scratch.select() end,          desc = "Select Scratch Buffer" },
-        { "<leader>nh",  function() Snacks.notifier.show_history() end,   desc = "Notification History" },
-        { "<leader>nd",  function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
-        { "<A-c>",       function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
-        { "<leader>rf",  function() Snacks.rename.rename_file() end,      desc = "Rename File" },
-        { "<c-/>",       function() Snacks.terminal() end,                desc = "Toggle Terminal" },
-        { "<c-_>",       function() Snacks.terminal() end,                desc = "which_key_ignore" },
-        { "]]",          function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",                mode = { "n", "t" } },
-        { "[[",          function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",                mode = { "n", "t" } },
-        { "<leader>gB",  function() Snacks.gitbrowse() end,               desc = "Git Browse" },
+        { "<leader>St", function() Snacks.scratch() end,                 desc = "Toggle Scratch Buffer" },
+        { "<leader>Ss", function() Snacks.scratch.select() end,          desc = "Select Scratch Buffer" },
+        { "<leader>nh", function() Snacks.notifier.show_history() end,   desc = "Notification History" },
+        { "<leader>nd", function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
+        { "<A-c>",      function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
+        { "<leader>rf", function() Snacks.rename.rename_file() end,      desc = "Rename File" },
+        { "<c-/>",      function() Snacks.terminal() end,                desc = "Toggle Terminal" },
+        { "<c-_>",      function() Snacks.terminal() end,                desc = "which_key_ignore" },
+        { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",              mode = { "n", "t" } },
+        { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",              mode = { "n", "t" } },
+        { "<leader>gx", function() Snacks.gitbrowse() end,               desc = "Git Browse" },
         -- LazyGit
-        { "<leader>gF",  function() Snacks.lazygit.log_file() end,        desc = "Lazygit Current File History" },
-        { "<leader>gg",  function() Snacks.lazygit.open() end,            desc = "Lazygit" },
-        { "<leader>gL",  function() Snacks.lazygit.log() end,             desc = "Lazygit Log (cwd)" },
+        { "<leader>gF", function() Snacks.lazygit.log_file() end,        desc = "Lazygit Current File History" },
+        { "<leader>gg", function() Snacks.lazygit.open() end,            desc = "Lazygit" },
+        { "<leader>gL", function() Snacks.lazygit.log() end,             desc = "Lazygit Log (cwd)" },
         -- [[ Picker ]]
         -- Search
-        { "<leader>sf",  function() Snacks.picker.files() end,            desc = "Files" },
-        { "<leader>sb",  function() Snacks.picker.buffers() end,          desc = "Buffers" },
-        { "<leader>sc",  function() Snacks.picker.commands() end,         desc = "Commands" },
-        { "<leader>sC",  function() Snacks.picker.command_history() end,  desc = "Command History" },
-        { "<leader>sd",  function() Snacks.picker.diagnostics() end,      desc = "Diagnostics" },
-        { "<leader>sm",  function() Snacks.picker.man() end,              desc = "Man Pages" },
-        { "<leader>sq",  function() Snacks.picker.qflist() end,           desc = "Quickfix List" },
-        { "<leader>sp",  function() Snacks.picker.projects() end,         desc = "Projects" },
+        { "<leader>sf", function() Snacks.picker.files() end,            desc = "Files" },
+        {
+            "<leader>sb",
+            function()
+                Snacks.picker.buffers({
+                    -- I always want my buffers picker to start in normal mode
+                    on_show = function()
+                        vim.cmd.stopinsert()
+                    end,
+                    win = {
+                        input = {
+                            keys = {
+                                ["d"] = "bufdelete",
+                            },
+                        },
+                        list = { keys = { ["d"] = "bufdelete" } },
+                    },
+                    -- In case you want to override the layout for this keymap
+                    -- layout = "ivy",
+                })
+            end,
+            desc = "Buffers"
+        },
+        { "<leader>sc", function() Snacks.picker.commands() end,           desc = "Commands" },
+        { "<leader>sC", function() Snacks.picker.command_history() end,    desc = "Command History" },
+        { "<leader>sd", function() Snacks.picker.diagnostics() end,        desc = "Diagnostics" },
         ---@diagnostic disable-next-line: undefined-field
-        { "<leader>st",  function() Snacks.picker.todo_comments() end,    desc = "Todo Comments" },
-        { "<leader>sr",  function() Snacks.picker.resume() end,           desc = "Resume" },
+        { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Diagnostics on Buffer" },
+        { "<leader>sp", function() Snacks.picker.projects() end,           desc = "Projects" },
+        ---@diagnostic disable-next-line: undefined-field
+        { "<leader>st", function() Snacks.picker.todo_comments() end,      desc = "Todo Comments" },
+        { "<leader>sr", function() Snacks.picker.resume() end,             desc = "Resume" },
         -- Grep
-        { "<leader>s/",  function() Snacks.picker.lines() end,            desc = "Grep Buffer Lines" },
-        { "<leader>sG",  function() Snacks.picker.grep_buffers() end,     desc = "Grep Open Buffers" },
-        { "<leader>sg",  function() Snacks.picker.grep() end,             desc = "Grep" },
-        { "<leader>sw",  function() Snacks.picker.grep_word() end,        desc = "Grep Visual selection or word", mode = { "n", "x" } },
+        { "<leader>s/", function() Snacks.picker.lines() end,              desc = "Grep Buffer Lines" },
+        { "<leader>sG", function() Snacks.picker.grep_buffers() end,       desc = "Grep Open Buffers" },
+        { "<leader>sg", function() Snacks.picker.grep() end,               desc = "Grep" },
+        { "<leader>sw", function() Snacks.picker.grep_word() end,          desc = "Grep Visual selection or word", mode = { "n", "x" } },
         -- Git
         -- - `<Tab>`: stages or unstages the currently selected file
         -- - `<cr>`: opens the currently selected file
-        { "<leader>gf",  function() Snacks.picker.git_files() end,        desc = "Find Git Files" },
-        { "<leader>gs",  function() Snacks.picker.git_status() end,       desc = "Git Status" },
-        { "<leader>gS",  function() Snacks.picker.git_stash() end,        desc = "Git Stash" },
-        { "<leader>gl",  function() Snacks.picker.git_log() end,          desc = "Git Log" },
+        {
+            "<leader>gb",
+            function()
+                Snacks.picker.git_branches({
+                    layout = "select",
+                })
+            end,
+            desc = "Git Branches"
+        },
+        { "<leader>gf",  function() Snacks.picker.git_files() end,  desc = "Find Git Files" },
+        { "<leader>gs",  function() Snacks.picker.git_status() end, desc = "Git Status" },
+        { "<leader>gS",  function() Snacks.picker.git_stash() end,  desc = "Git Stash" },
+        { "<leader>gl",  function() Snacks.picker.git_log() end,    desc = "Git Log" },
         -- Neovim
-        { "<leader>snh", function() Snacks.picker.help() end,             desc = "Help" },
-        { "<leader>snk", function() Snacks.picker.keymaps() end,          desc = "Keymaps" },
+        { "<leader>snh", function() Snacks.picker.help() end,       desc = "Help" },
+        {
+            "<leader>snk",
+            function()
+                Snacks.picker.keymaps({
+                    layout = 'vertical',
+                })
+            end,
+            desc = "Keymaps"
+        },
         {
             "<leader>snf",
             function()
