61692e1d00c350736113e19e296077b747d06d24
Umut Sahin Onder <umutsonder@gmail.com>
2025-02-07 17:58:33 +0100

    updated lualine in case if i use it

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -45,51 +45,47 @@ return {
       vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
     enabled = false,
-    config = function()
-      require 'lualine'.setup {
-        options = {
-          icons_enabled = true,
-          component_separators = { left = '', right = '|' },
-          section_separators = { left = '', right = '' },
-          disabled_filetypes = { 'NvimTree' },
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        },
-        sections = {
-          lualine_a = { 'mode' },
-          lualine_b = { 'branch', 'diff', 'diagnostics' },
-          lualine_c = { 'filename' },
-          -- lualine_x = { 'encoding', 'fileformat', 'filetype' },
-          lualine_x = { 'filetype' },
-          lualine_y = {},
-          lualine_z = { 'location' },
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { 'filename' },
-          lualine_x = { 'location' },
-          lualine_y = {},
-          lualine_z = {},
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {},
-      }
-    end,
+    opts = {
+      options = {
+        icons_enabled = true,
+        component_separators = { left = '', right = '|' },
+        section_separators = { left = '', right = '' },
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = true,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+      },
+      sections = {
+        lualine_a = {},
+        lualine_b = { 'diff', 'diagnostics' },
+        lualine_c = { 'buffers' },
+        -- lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_x = { 'filetype' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {},
+    }
   },
 
 }
