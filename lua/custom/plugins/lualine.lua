0f847fa4e03395c47c802e204e14f3f819448445
Umut Sahin Onder <umutsonder@gmail.com>
2025-02-19 18:27:56 +0100

    moved icons to its own file

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -134,74 +134,7 @@ return {
         local lualine_require = require("lualine_require")
         lualine_require.require = require
 
-        local icons = {
-            misc = {
-                dots = "󰇘",
-            },
-            ft = {
-                octo = "",
-            },
-            dap = {
-                Stopped             = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
-                Breakpoint          = " ",
-                BreakpointCondition = " ",
-                BreakpointRejected  = { " ", "DiagnosticError" },
-                LogPoint            = ".>",
-            },
-            diagnostics = {
-                Error = " ",
-                Warn  = " ",
-                Hint  = " ",
-                Info  = " ",
-            },
-            git = {
-                added    = " ",
-                modified = " ",
-                removed  = " ",
-            },
-            kinds = {
-                Array         = " ",
-                Boolean       = "󰨙 ",
-                Class         = " ",
-                Codeium       = "󰘦 ",
-                Color         = " ",
-                Control       = " ",
-                Collapsed     = " ",
-                Constant      = "󰏿 ",
-                Constructor   = " ",
-                Copilot       = " ",
-                Enum          = " ",
-                EnumMember    = " ",
-                Event         = " ",
-                Field         = " ",
-                File          = " ",
-                Folder        = " ",
-                Function      = "󰊕 ",
-                Interface     = " ",
-                Key           = " ",
-                Keyword       = " ",
-                Method        = "󰊕 ",
-                Module        = " ",
-                Namespace     = "󰦮 ",
-                Null          = " ",
-                Number        = "󰎠 ",
-                Object        = " ",
-                Operator      = " ",
-                Package       = " ",
-                Property      = " ",
-                Reference     = " ",
-                Snippet       = "󱄽 ",
-                String        = " ",
-                Struct        = "󰆼 ",
-                Supermaven    = " ",
-                TabNine       = "󰏚 ",
-                Text          = " ",
-                TypeParameter = " ",
-                Unit          = " ",
-                Value         = " ",
-                Variable      = "󰀫 ",
-            }
-        }
+        local icons = require('kickstart.icons')
 
         local opts = {
             options = {
