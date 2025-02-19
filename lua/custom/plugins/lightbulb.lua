876320fef285a4eb8f679e40d93282255b370b3c
Umut Sahin Onder <umutsonder@gmail.com>
2025-02-19 22:05:20 +0100

    made lightbulb ignore gopls since all the lines have code actions

--- lua/custom/plugins/lightbulb.lua
+++ lua/custom/plugins/lightbulb.lua
@@ -5,11 +5,9 @@ return {
         number = { enabled = true },
         sign = {
             enabled = true,
-            -- Text to show in the sign column.
-            -- Must be between 1-2 characters.
             text = "",
-            -- Highlight group to highlight the sign column text.
             hl = "LightBulbSign",
-        }
+        },
+        ignore = { clients = { 'gopls' } }
     }
 }
