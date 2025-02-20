b97c33c08a1693e637749aa3967a96d03651b165
Umut Sahin Onder <umutsonder@gmail.com>
2025-02-20 01:54:40 +0100

    updated nvim-lint config

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -4,28 +4,42 @@ return {
     "BufReadPre",
     "BufNewFile",
   },
-  config = function()
-    local lint = require("lint")
-
+  opts = {
     --NOTE: add the linters to ensure_installed with add_tools function
-    lint.linters_by_ft = {
+    linters_by_ft = {
       javascript = { "eslint_d" },
       typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
       python = { "flake8" },
       go = { "golangcilint" },
+      fish = { "fish" }
     }
+  },
+  config = function(_, opts)
+    local lint = require("lint")
+    lint.linters_by_ft = opts.linters_by_ft
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    local function debounce(ms, fn)
+      local timer = vim.uv.new_timer()
+      return function(...)
+        local argv = { ... }
+        if fn and timer then
+          timer:start(ms, 0, function()
+            timer:stop()
+            vim.schedule_wrap(fn)(unpack(argv))
+          end)
+        end
+      end
+    end
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
+    vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "InsertLeave" }, {
+      group = vim.api.nvim_create_augroup("lint", { clear = true }),
+      callback = debounce(100, function()
         if vim.opt_local.modifiable:get() then
           lint.try_lint()
         end
-      end,
+      end)
     })
   end,
 }
