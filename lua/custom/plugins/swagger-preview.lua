f5058187f184d83218f7e841b140a14c6d4491d3
Umut Sahin Onder <umutsonder@gmail.com>
2025-02-20 18:47:21 +0100

    enabled swagger-preview when its not a dev container

--- lua/custom/plugins/swagger-preview.lua
+++ lua/custom/plugins/swagger-preview.lua
@@ -1,6 +1,6 @@
 return {
     "vinnymeller/swagger-preview.nvim",
-    enabled = false,
+    enabled = vim.fn.getenv("REMOTE_CONTAINERS") ~= 'true',
     build = "npm install -g swagger-ui-watcher",
     cmd = { "SwaggerPreview", "SwaggerPreviewStop", "SwaggerPreviewToggle" },
     opts = {},
