fd421bd898c824cac1bbc2f1532989f995932814
Umut Sahin Onder <umutsonder@gmail.com>
2025-03-11 02:49:45 +0100

    added json-to-types plugin and implemented it with kulala

--- lua/custom/plugins/kulala.lua
+++ lua/custom/plugins/kulala.lua
@@ -2,6 +2,14 @@ return {
     'mistweaverco/kulala.nvim',
     opts = {},
     ft = 'http',
+    dependencies = {
+        {
+            "Redoxahmii/json-to-types.nvim",
+            build = "sh install.sh npm",
+            ft = "json",
+            cmd = 'ConvertJSONtoLang'
+        }
+    },
     init = function()
         local commands = {
             Run = "run",
@@ -17,21 +25,79 @@ return {
             Search = "search",
             JumpPrev = "jump_prev",
             JumpNext = "jump_next",
+            TypeDef = "TypeDef" -- Add Type as a subcommand
         }
         vim.api.nvim_create_user_command("Kulala", function(opts)
             local kulala = require("kulala")
-            local subcommand = commands[opts.args]
+            local args = vim.split(opts.args, " ")
+            local subcommand = commands[args[1]]
 
-            if subcommand and kulala[subcommand] then
+            if subcommand == "TypeDef" then
+                if not args[2] then
+                    vim.notify('need to specify language')
+                    return
+                end
+                require("kulala.api").on("after_next_request", function(data)
+                    local filename = "kulala.json"
+                    local file = io.open(filename, "w")
+                    if not file then
+                        print("Error: Failed to open file for writing")
+                        return
+                    end
+                    file:write(data.body)
+                    file:close()
+                    vim.cmd("edit " .. filename)
+                    local buf = vim.api.nvim_get_current_buf()
+                    vim.cmd("ConvertJSONtoLang " .. args[2])
+
+                    vim.api.nvim_buf_delete(buf, { force = true })
+                    os.remove(filename)
+                end)
+                require("kulala").run()
+            elseif subcommand and kulala[subcommand] then
                 kulala[subcommand]()
             else
                 print("Unknown Kulala command: " .. opts.args)
             end
         end, {
-            nargs = 1,
-            complete = function()
-                return vim.tbl_keys(commands) -- Auto-completion from command keys
+            nargs = "+",
+            complete = function(_, cmdline, _)
+                local parts = vim.split(cmdline, " ")
+                if #parts == 2 then
+                    return vim.tbl_keys(commands) -- Auto-completion for subcommands
+                elseif #parts == 3 and parts[2] == "TypeDef" then
+                    return {                      -- Auto-completion for languages
+                        "typescript",
+                        "javascript",
+                        "python",
+                        "csharp",
+                        "go",
+                        "rust",
+                        "php",
+                        "ruby",
+                        "swift",
+                        "kotlin",
+                        "cpp",
+                        "cjson",
+                        "cr",
+                        "dart",
+                        "elixir",
+                        "elm",
+                        "flow",
+                        "haskell",
+                        "java",
+                        "javascript-prop-types",
+                        "objc",
+                        "pike",
+                        "scala3",
+                        "Smithy",
+                        "typescript-zod",
+                        "typescript-effect-schema"
+                    }
+                end
+                return {}
             end,
         })
     end
+
 }
