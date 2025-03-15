fa6ef39681805d7c092789c47cb6b04d3f4d056b
Umut Sahin Onder <umutsonder@gmail.com>
2025-03-15 01:09:14 +0100

    mini.ai cannot select around functions so I added back treesitter select

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -47,6 +47,15 @@ return {
       },
     },
     textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['af'] = { query = '@function.outer', desc = "around function", },
+          ['if'] = { query = '@function.inner', desc = "inside function", },
+        }
+      },
       move = {
         enable = true,
         goto_next_start = { [']f'] = { query = '@function.outer', desc = 'Function forward' } },
