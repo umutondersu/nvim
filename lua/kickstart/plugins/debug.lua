f6752a24c1e2354fc4e5d1cd5cab1a7aaa11dc46
Umut Sahin Onder <umutsonder@gmail.com>
2025-03-21 17:05:26 +0100

    Reconfigured debug.lua

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -1,11 +1,9 @@
--- debug.lua
--- Shows how to use the DAP plugin to debug your code.
 local ft = { 'go', 'python' }
 return {
   'mfussenegger/nvim-dap',
   dependencies = {
     -- Creates a beautiful debugger UI
-    { 'rcarriga/nvim-dap-ui', dependencies = { 'nvim-neotest/nvim-nio' } },
+    'rcarriga/nvim-dap-ui',
 
     -- Inline virtual text for debugging
     'theHamsta/nvim-dap-virtual-text',
@@ -16,14 +14,16 @@ return {
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
-    { '<leader>cdr', function() require('dap').continue() end,                                            desc = 'Run/Continue',             ft = ft },
-    { '<leader>cdi', function() require('dap').step_into() end,                                           desc = 'Step Into',                ft = ft },
-    { '<leader>cdo', function() require('dap').step_over() end,                                           desc = 'Step Over',                ft = ft },
-    { '<leader>cdO', function() require('dap').step_out() end,                                            desc = 'Step Out',                 ft = ft },
-    { '<leader>cdt', function() require('dap').toggle_breakpoint() end,                                   desc = 'Toggle Breakpoint',        ft = ft },
-    { '<leader>cds', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Set Breakpoint',           ft = ft },
-    { '<Leader>cdp', function() require('dap').pause() end,                                               desc = 'Pause',                    ft = ft },
-    { '<Leader>cdR', function() require('dapui').toggle() end,                                            desc = 'See last session result.', ft = ft },
+    { '<leader>cdr', function() require('dap').continue() end,                                            desc = 'Run/Continue',               ft = ft },
+    { '<leader>cdi', function() require('dap').step_into() end,                                           desc = 'Step Into',                  ft = ft },
+    { '<leader>cdo', function() require('dap').step_over() end,                                           desc = 'Step Over',                  ft = ft },
+    { '<leader>cdO', function() require('dap').step_out() end,                                            desc = 'Step Out',                   ft = ft },
+    { '<leader>cdb', function() require('dap').toggle_breakpoint() end,                                   desc = 'Toggle Breakpoint',          ft = ft },
+    { '<leader>cdB', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Set Breakpoint w/condition', ft = ft },
+    { '<Leader>cdp', function() require('dap').pause() end,                                               desc = 'Pause',                      ft = ft },
+    { '<leader>cdt', function() require('dap').terminate() end,                                           desc = 'Terminate',                  ft = ft },
+    { '<Leader>cdR', function() require('dapui').toggle() end,                                            desc = 'Last Session Results',       ft = ft },
+    { '<leader>cde', function() require('dapui').eval() end,                                              desc = 'Eval',                       ft = ft, mode = { "n", "v" } },
   },
   config = function()
     local dap = require 'dap'
@@ -58,13 +58,12 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     -- Install language specific configurations
-    local is_windows = vim.fn.has 'win32'
-    local nvim_data_path = '~/.local/share/nvim'
+    local is_windows = vim.fn.has('win32') == 1
+    local nvim_data_path = is_windows
+        and vim.fn.expand('%LOCALAPPDATA%/nvim-data')
+        or vim.fn.expand('~/.local/share/nvim')
 
     -- Python
-    if is_windows then
-      nvim_data_path = '%LOCALAPPDATA%/nvim-data'
-    end
     require('dap-python').setup(nvim_data_path .. '/mason/packages/debugpy/venv/bin/python')
 
     -- Go
