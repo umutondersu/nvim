a7d0a930d94b4c1383c200c7f7846f834ddaad10
Umut Sahin Onder <umutsonder@gmail.com>
2025-03-31 17:47:18 +0200

    enable a formatter for langages in the disable_filetypes table to have a formatter that can be run manually but doesnt enable format_on_save for them

--- lua/kickstart/plugins/autoformat.lua
+++ lua/kickstart/plugins/autoformat.lua
@@ -42,13 +42,14 @@ return
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
       local disable_filetypes = { c = true, cpp = true }
-      local lsp_format_opt
       if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = 'never'
+        return nil
       else
-        lsp_format_opt = 'fallback'
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
       end
-      return { timeout_ms = 500, lsp_format = lsp_format_opt }
     end,
     --NOTE: add the formatters to ensure_installed with add_tools function
     formatters_by_ft = {
