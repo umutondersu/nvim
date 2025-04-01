2cec74607e6126ffcb7156cd124e562efc372cb4
Umut Sahin Onder <umutsonder@gmail.com>
2025-04-02 01:10:40 +0200

    use import in ui insted of require

--- lua/kickstart/plugins/ui.lua
+++ lua/kickstart/plugins/ui.lua
@@ -1,9 +1,1 @@
-return {
-    require('kickstart.plugins.ui.bufferline'),
-    require('kickstart.plugins.ui.colorscheme'),
-    require('kickstart.plugins.ui.dropbar'),
-    require('kickstart.plugins.ui.lightbulb'),
-    require('kickstart.plugins.ui.noice'),
-    require('kickstart.plugins.ui.rainbow-delimeter'),
-    require('kickstart.plugins.ui.tiny-inline-diagnostic'),
-}
+return { import = 'kickstart.plugins.ui' }
