6072bf4609238bac5f6b69e86547de2084d7b459
Umut Sahin Onder <umutsonder@gmail.com>
2025-04-04 16:57:52 +0200

    Changed gitsigns

--- lua/kickstart/plugins/git.lua
+++ lua/kickstart/plugins/git.lua
@@ -32,7 +32,21 @@ return {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" }
+      },
+      signs_staged = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+      },
       on_attach = function(bufnr)
         local gs = require 'gitsigns'
 
