cc760e51c9d8059a503dff0bf6fd7f3542ca705a
Umut Sahin Onder <umutsonder@gmail.com>
2025-04-09 23:52:57 +0200

    Fix treesitter-context not loading

--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -1,12 +1,11 @@
 return {
     'nvim-treesitter/nvim-treesitter-context',
     dependencies = 'nvim-treesitter/nvim-treesitter',
-    keys = {
-        {
-            'gC',
+    event = 'VeryLazy',
+    config = function()
+        vim.keymap.set('n', 'gC',
             function() require('treesitter-context').go_to_context(vim.v.count1) end,
-            mode = 'n',
-            desc = 'Jump to context',
-        },
-    },
+            { desc = 'Jump to context' }
+        )
+    end,
 }
