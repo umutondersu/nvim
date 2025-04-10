99c33c26001f2b8aaa5162564d061bdbb0881e18
Umut Sahin Onder <umutsonder@gmail.com>
2025-04-10 16:39:13 +0200

    Update tiny-inline-diagnostic config

--- lua/kickstart/plugins/ui/tiny-inline-diagnostic.lua
+++ lua/kickstart/plugins/ui/tiny-inline-diagnostic.lua
@@ -4,7 +4,12 @@ return {
     priority = 1000, -- needs to be loaded in first
     opts = {
         preset = 'amongus',
-        options = { show_source = true },
+        options = {
+            show_source = {
+                enabled = true, -- enable showing sources
+                if_many = true  -- only show when multiple different sources exist
+            }
+        },
         hi = { background = "None" }
     },
     init = function() vim.diagnostic.config { virtual_text = false } end
