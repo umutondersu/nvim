a1203e89a7b10bb2693b43b768e0d9bbbb92ac1f
Umut Sahin Onder <umutsonder@gmail.com>
2025-04-25 13:38:43 +0200

    Update swagger-preview config

--- lua/custom/plugins/swagger-preview.lua
+++ lua/custom/plugins/swagger-preview.lua
@@ -1,8 +1,7 @@
 return {
     "vinnymeller/swagger-preview.nvim",
     enabled = vim.fn.getenv("REMOTE_CONTAINERS") ~= 'true',
-    build = "npm install -g swagger-ui-watcher",
+    build = "npm i",
     cmd = { "SwaggerPreview", "SwaggerPreviewStop", "SwaggerPreviewToggle" },
-    opts = {},
-    ft = "yaml"
+    opts = {}
 }
