5d571bb7705acfdb37a5a68734383550c5117a98
Umut Sahin Onder <umutsonder@gmail.com>
2025-05-10 22:31:35 +0200

    Remove wit.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,9 +3,8 @@ return {
     'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
     'tpope/vim-repeat', -- Make most of the plugins repeatable with .
     { 'anuvyklack/help-vsplit.nvim',                 opts = {} },
-    { 'numToStr/Comment.nvim',                       opts = {},                              event = 'BufReadPost' },
+    { 'numToStr/Comment.nvim',                       opts = {},                             event = 'BufReadPost' },
     { 'JoosepAlviste/nvim-ts-context-commentstring', dependencies = 'numToStr/Comment.nvim' },
     { 'danitrap/cheatsh.nvim',                       cmd = { 'CheatSh' } },
-    { 'aliqyan-21/wit.nvim',                         cmd = { 'WitSearch', 'WitSearchWiki' }, opts = {} },
-    { 'max397574/colortils.nvim',                    cmd = 'Colortils',                      opts = {} },
+    { 'max397574/colortils.nvim',                    cmd = 'Colortils',                     opts = {} },
 }
