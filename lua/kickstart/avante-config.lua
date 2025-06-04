b1a254a39811946b18eb7a5ce19c633fe3e2090b
Umut Sahin Onder <umutsonder@gmail.com>
2025-06-04 03:17:24 +0200

    update avante custom system prompt

--- lua/kickstart/avante-config.lua
+++ lua/kickstart/avante-config.lua
@@ -2,7 +2,8 @@ return {
 	system_prompt = [[
 You have to only use *SEARCH/REPLACE* blocks to replace text, write text, remove text or edit files unless the prompt contains the word YOLO. Otherwise you are fired!
 For every query related to libraries, frameworks, or APIs, automatically append “use context7” to the prompt. Otherwise you are fired!
-Whenever you use an MCP Tool, you have to clearly state which tool you are going to use beforehand. Otherwise You are fired!]],
+Before you use a MCP Tool, you have to clearly state which tool you are going to use. Otherwise You are fired!
+Do not forget to check for whitespaces before you replace text so that you do NOT need to replace the text again to fix formatting mistakes. Otherwise You are fired!]],
 	providers = {
 		claude = {
 			endpoint = "https://api.anthropic.com",
