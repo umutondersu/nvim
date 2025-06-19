0697797002ead07d125231f3d9daa2644f47f61c
Umut Sahin Onder <umutsonder@gmail.com>
2025-06-19 23:22:23 +0200

    remove unused tailwind keymaps

--- lua/kickstart/plugins/language/tailwind/tailwind.lua
+++ lua/kickstart/plugins/language/tailwind/tailwind.lua
@@ -18,19 +18,16 @@ return {
 	},
 	opts = {},
 	keys = {
-		{ "<leader>ct",  function() end,                                     desc = "Tailwind",                ft = tw_ft },
+		{ "<leader>ct",  function() end,                                 desc = "Tailwind",                ft = tw_ft },
 		-- Conceal toggles
-		{ "<leader>ctc", function() vim.cmd.TailwindConcealToggle() end,     desc = "Toggle Tailwind conceal", ft = tw_ft },
+		{ "<leader>ctc", function() vim.cmd.TailwindConcealToggle() end, desc = "Toggle Tailwind conceal", ft = tw_ft },
 		-- Color hints toggles
-		{ "<leader>ctC", function() vim.cmd.TailwindColorToggle() end,       desc = "Toggle Tailwind colors",  ft = tw_ft },
-		-- Class sorting
-		{ "<leader>cts", function() vim.cmd.TailwindSortSync() end,          desc = "Sort Tailwind classes",   ft = tw_ft },
-		{ "<leader>cts", function() vim.cmd.TailwindSortSelectionSync() end, desc = "Sort Tailwind classes",   ft = tw_ft, mode = 'v' },
+		{ "<leader>ctC", function() vim.cmd.TailwindColorToggle() end,   desc = "Toggle Tailwind colors",  ft = tw_ft },
 		-- Class navigation
-		{ "]t",          function() vim.cmd.TailwindNextClass() end,         desc = "Next Tailwind class",     ft = tw_ft },
-		{ "[t",          function() vim.cmd.TailwindPrevClass() end,         desc = "Previous Tailwind class", ft = tw_ft },
-		{ "[T",          function() vim.cmd.TailwindNextClass() end,         desc = "Next Tailwind class",     ft = tw_ft },
-		{ "]T",          function() vim.cmd.TailwindPrevClass() end,         desc = "Previous Tailwind class", ft = tw_ft },
+		{ "]t",          function() vim.cmd.TailwindNextClass() end,     desc = "Next Tailwind class",     ft = tw_ft },
+		{ "[t",          function() vim.cmd.TailwindPrevClass() end,     desc = "Previous Tailwind class", ft = tw_ft },
+		{ "[T",          function() vim.cmd.TailwindNextClass() end,     desc = "Next Tailwind class",     ft = tw_ft },
+		{ "]T",          function() vim.cmd.TailwindPrevClass() end,     desc = "Previous Tailwind class", ft = tw_ft },
 
 	}
 }
