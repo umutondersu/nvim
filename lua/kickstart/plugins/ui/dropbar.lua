9756d560efcccdb183a2c3dbc36997b52579e3b3
Umut Sahin Onder <umutsonder@gmail.com>
2025-07-08 21:06:59 +0200

    replace init with config in dropbar

--- lua/kickstart/plugins/ui/dropbar.lua
+++ lua/kickstart/plugins/ui/dropbar.lua
@@ -20,7 +20,7 @@ return {
 			}
 		}
 	},
-	init = function()
+	config = function()
 		vim.keymap.set('n', '<m-d>', [[<cmd>lua require('dropbar.api').pick()<cr>]], { desc = 'Pick symbols in winbar' })
 	end,
 }
