f8626a7e98c583de11aa037a4d70b6679a4fd7f4
Umut Sahin Onder <umutsonder@gmail.com>
2025-07-13 19:25:09 +0200

    Update flash keymaps

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -3,8 +3,8 @@ return {
     event = "VeryLazy",
     opts = {},
     keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash Jump" },
-        { "S", mode = { "n", "o" },      function() require("flash").treesitter() end,        desc = "Flash Treesitter Selection" },
+        { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash Jump" },
+        { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter Selection" },
         { "r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
         { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     },
