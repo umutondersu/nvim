84b43bc994ecc403f5606fc728ee09cfc683f167
Umut Sahin Onder <umutsonder@gmail.com>
2025-07-14 22:26:12 +0200

    add keymap for save and exit

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -104,6 +104,7 @@ map('n', '<leader>w', function()
 		vim.cmd.write()
 	end
 end, { desc = 'Save Buffer' })
+map('n', '<C-M-x>', '<cmd>wqa<CR>', { desc = 'Save and Exit' })
 map('n', '<leader>q', '<cmd>q<CR>', { desc = 'Quit Window' })
 
 -- Refactor Keymaps
