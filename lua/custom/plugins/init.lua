36c98c971783569fd78d72745f6662e432942874
Umut Sahin Onder <umutsonder@gmail.com>
2025-07-16 04:16:58 +0200

    Lazy load nvim-ts-context-commentstring

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,8 +3,8 @@ return {
     'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
     'tpope/vim-repeat', -- Make most of the plugins repeatable with .
     { 'anuvyklack/help-vsplit.nvim',                 opts = {} },
-    { 'numToStr/Comment.nvim',                       opts = {},                             event = 'BufReadPost' },
-    { 'JoosepAlviste/nvim-ts-context-commentstring', dependencies = 'numToStr/Comment.nvim' },
-    { 'danitrap/cheatsh.nvim',                       cmd = { 'CheatSh' } },
-    { 'max397574/colortils.nvim',                    cmd = 'Colortils',                     opts = {} },
+    { 'numToStr/Comment.nvim',                       event = 'BufReadPost', opts = {} },
+    { 'JoosepAlviste/nvim-ts-context-commentstring', event = 'InsertEnter' },
+    { 'danitrap/cheatsh.nvim',                       cmd = 'CheatSh' },
+    { 'max397574/colortils.nvim',                    cmd = 'Colortils',     opts = {} },
 }
