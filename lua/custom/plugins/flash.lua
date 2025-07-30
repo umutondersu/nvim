b5e26083359f88f28eb0ae4104b4a0e070269271
Umut Sahin Onder <umutsonder@gmail.com>
2025-07-30 03:14:46 +0200

    Update flash.nvim

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -1,11 +1,17 @@
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
-    keys = {
-        { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash Jump" },
-        { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter Selection" },
-        { "r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-        { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    opts = {
+        modes = {
+            search = { enabled = true },
+            char = { jump_labels = true }
+        }
     },
+    keys = {
+        { "s",     mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash Jump" },
+        { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter Selection" },
+        { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+        { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = "c",               function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    }
 }
