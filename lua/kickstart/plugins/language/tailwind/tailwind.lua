f8cb8bbd144d6486edf1fc1228915bb64b91dfdf
Umut Sahin Onder <umutsonder@gmail.com>
2025-10-22 01:15:01 +0200

    add tailwind-sorter.nvim

--- lua/kickstart/plugins/language/tailwind/tailwind.lua
+++ lua/kickstart/plugins/language/tailwind/tailwind.lua
@@ -9,27 +9,38 @@ local tw_ft = {
 	"markdown",
 }
 return {
-	"luckasRanarison/tailwind-tools.nvim",
-	build = ":UpdateRemotePlugins",
-	ft = tw_ft,
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"neovim/nvim-lspconfig",
-	},
-	opts = {
-		server = { override = false },
-	},
-	keys = {
-		{ "<leader>ct",  function() end,                                 desc = "Tailwind",                ft = tw_ft },
-		-- Conceal toggles
-		{ "<leader>ctc", function() vim.cmd.TailwindConcealToggle() end, desc = "Toggle Tailwind conceal", ft = tw_ft },
-		-- Color hints toggles
-		{ "<leader>ctC", function() vim.cmd.TailwindColorToggle() end,   desc = "Toggle Tailwind colors",  ft = tw_ft },
-		-- Class navigation
-		{ "]t",          function() vim.cmd.TailwindNextClass() end,     desc = "Next Tailwind class",     ft = tw_ft },
-		{ "[t",          function() vim.cmd.TailwindPrevClass() end,     desc = "Previous Tailwind class", ft = tw_ft },
-		{ "[T",          function() vim.cmd.TailwindNextClass() end,     desc = "Next Tailwind class",     ft = tw_ft },
-		{ "]T",          function() vim.cmd.TailwindPrevClass() end,     desc = "Previous Tailwind class", ft = tw_ft },
+	{
+		"luckasRanarison/tailwind-tools.nvim",
+		build = ":UpdateRemotePlugins",
+		ft = tw_ft,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			server = { override = false },
+		},
+		keys = {
+			{ "<leader>ct",  function() end,                                 desc = "Tailwind",                ft = tw_ft },
+			-- Conceal toggles
+			{ "<leader>ctc", function() vim.cmd.TailwindConcealToggle() end, desc = "Toggle Tailwind conceal", ft = tw_ft },
+			-- Color hints toggles
+			{ "<leader>ctC", function() vim.cmd.TailwindColorToggle() end,   desc = "Toggle Tailwind colors",  ft = tw_ft },
+			-- Class navigation
+			{ "]t",          function() vim.cmd.TailwindNextClass() end,     desc = "Next Tailwind class",     ft = tw_ft },
+			{ "[t",          function() vim.cmd.TailwindPrevClass() end,     desc = "Previous Tailwind class", ft = tw_ft },
+			{ "[T",          function() vim.cmd.TailwindNextClass() end,     desc = "Next Tailwind class",     ft = tw_ft },
+			{ "]T",          function() vim.cmd.TailwindPrevClass() end,     desc = "Previous Tailwind class", ft = tw_ft },
 
+		}
+	},
+	{
+		"laytan/tailwind-sorter.nvim",
+		ft = tw_ft,
+		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+		opts = {
+			on_save_enabled = true, -- If `true`, automatically enables on save sorting.
+			trim_spaces = true, -- If `true`, trim any extra spaces after sorting.
+		}
 	}
 }
