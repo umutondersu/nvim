9a707ce792f11888bc0f992d91b05fcd36c40592
Umut Sahin Onder <umutsonder@gmail.com>
2026-03-31 14:16:55 +0200

    update mini.ai config

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -7,13 +7,33 @@ return {
     --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
     --  - ci'  - [C]hange [I]nside [']quote
     {
-        'nvim-mini/mini.ai',
-        version = false,
-        event = 'VeryLazy',
-        opts = {
-            n_lines = 500,
-            mappings = { around_last = 'ap', inside_last = 'ip', search_method = 'cover' },
-        }
+        "nvim-mini/mini.ai",
+        event = "VeryLazy",
+        opts = function()
+            local ai = require("mini.ai")
+            return {
+                n_lines = 500,
+                custom_textobjects = {
+                    o = ai.gen_spec.treesitter({ -- code block
+                        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                        i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+                    }),
+                    f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+                    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),       -- class
+                    t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },           -- tags
+                    d = { "%f[%d]%d+" },                                                          -- digits
+                    e = {                                                                         -- Word with case
+                        { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+                        "^().*()$",
+                    },
+                    u = ai.gen_spec.function_call(),                           -- u for "Usage"
+                    U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+                },
+            }
+        end,
+        config = function(_, opts)
+            require("mini.ai").setup(opts)
+        end,
     },
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     --
