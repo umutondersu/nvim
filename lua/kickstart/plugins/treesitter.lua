45bb4195d35209b847f576cd3fe47164bc132ac3
Umut Sahin Onder <umutsonder@gmail.com>
2026-04-02 05:12:40 +0200

    shamelessly stole treesitter config from kickstart

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -1,135 +1,46 @@
 return {
-    -- Treesitter is a new parser generator tool that we can
-    -- use in Neovim to power faster and more accurate
-    -- syntax highlighting.
-    {
+    { -- Highlight, edit, and navigate code
         "nvim-treesitter/nvim-treesitter",
+        lazy = false,
         branch = "main",
-        build = function()
-            local TS = require("nvim-treesitter")
-            if not TS.get_installed then
-                vim.notify("Please restart Neovim and run `:TSUpdate` to use the `nvim-treesitter` **main** branch.",
-                    vim.log.levels.ERROR)
-                return
-            end
-            if not vim.fn.executable('tree-sitter') then
-                vim.notify("tree-sitter CLI is not installed!", vim.log.levels.ERROR)
-                return
-            end
-        end,
-        event = "VeryLazy",
-        cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
-        opts_extend = { "ensure_installed" },
-        opts = {
-            indent = { enable = true },
-            highlight = { enable = true },
-            folds = { enable = true },
-            ensure_installed = {
-                'html',
-                'css',
-                'scss',
-                'c',
-                'cpp',
-                'go',
-                'lua',
-                'python',
-                'fish',
-                'tsx',
-                'javascript',
-                'typescript',
-                'vimdoc',
-                'vim',
-                'bash',
-                'http',
-                'json',
-                'sql',
-                'markdown',
-                'markdown_inline', -- noice.nvim
-                'regex',           -- noice.nvim
-                'diff',
-                'gitcommit'
-            },
-        },
-        config = function(_, opts)
-            local TS = require("nvim-treesitter")
-            setmetatable(require("nvim-treesitter.install"), {
-                __newindex = function(_, k)
-                    if k == "compilers" then
-                        vim.schedule(function()
-                            vim.notify(
-                                [[Setting custom compilers for `nvim-treesitter` is no longer supported.
-                                For more info, see:,
-                                - [compilers](https://docs.rs/cc/latest/cc/#compile-time-requirements)]]
-                            )
-                        end)
-                    end
-                end,
-            })
+        build = ":TSUpdate",
+        config = function()
+            -- ensure basic parser are installed
+            local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim',
+                'vimdoc', 'regex' }
+            require('nvim-treesitter').install(parsers)
 
-            -- some quick sanity checks
-            if not TS.get_installed then
-                return vim.notify("Please use `:Lazy` and update `nvim-treesitter`", vim.log.levels.ERROR)
-            elseif type(opts.ensure_installed) ~= "table" then
-                return vim.notify("`nvim-treesitter` opts.ensure_installed must be a table", vim.log.levels.ERROR)
+            ---@param buf integer
+            ---@param language string
+            local function treesitter_try_attach(buf, language)
+                -- check if parser exists and load it
+                if not vim.treesitter.language.add(language) then return end
+                -- enables syntax highlighting and other treesitter features
+                vim.treesitter.start(buf, language)
+                -- enables treesitter based indentation
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
             end
 
-            local treesitter = {}
-            treesitter._installed = nil
-            treesitter._queries = {}
-            local function get_installed(update)
-                if update then
-                    treesitter._installed, treesitter._queries = {}, {}
-                    for _, lang in ipairs(TS.get_installed("parsers")) do
-                        treesitter._installed[lang] = true
-                    end
-                end
-                return treesitter._installed or {}
-            end
-            local function have_query(lang, query)
-                local key = lang .. ":" .. query
-                if treesitter._queries[key] == nil then
-                    treesitter._queries[key] = vim.treesitter.query.get(lang, query) ~= nil
-                end
-                return treesitter._queries[key]
-            end
-            local function have(what, query)
-                what = what or vim.api.nvim_get_current_buf()
-                what = type(what) == "number" and vim.bo[what].filetype or what
-                local lang = vim.treesitter.language.get_lang(what)
-                if lang == nil or get_installed()[lang] == nil then
-                    return false
-                end
-                if query and not have_query(lang, query) then
-                    return false
-                end
-                return true
-            end
+            local available_parsers = require('nvim-treesitter').get_available()
+            vim.api.nvim_create_autocmd('FileType', {
+                group = vim.api.nvim_create_augroup('treesitter-attach', { clear = true }),
+                callback = function(args)
+                    local buf, filetype = args.buf, args.match
 
-            TS.setup(opts)
-            get_installed(true)
-            local install = vim.tbl_filter(function(lang)
-                return not have(lang)
-            end, opts.ensure_installed or {})
-            if #install > 0 then
-                TS.install(install, { summary = true }):await(function()
-                    get_installed(true)
-                end)
-            end
-            vim.api.nvim_create_autocmd("FileType", {
-                group = vim.api.nvim_create_augroup("my_treesitter", { clear = true }),
-                callback = function(ev)
-                    local ft, lang = ev.match, vim.treesitter.language.get_lang(ev.match)
-                    if not have(ft) then
-                        return
-                    end
-                    local function enabled(feat, query)
-                        local f = opts[feat] or {}
-                        return f.enable ~= false
-                            and not (type(f.disable) == "table" and vim.tbl_contains(f.disable, lang))
-                            and have(ft, query)
-                    end
-                    if enabled("highlight", "highlights") then
-                        pcall(vim.treesitter.start, ev.buf)
+                    local language = vim.treesitter.language.get_lang(filetype)
+                    if not language then return end
+
+                    local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
+
+                    if vim.tbl_contains(installed_parsers, language) then
+                        -- enable the parser if it is installed
+                        treesitter_try_attach(buf, language)
+                    elseif vim.tbl_contains(available_parsers, language) then
+                        -- if a parser is available in `nvim-treesitter` auto install it, and enable it after the installation is done
+                        require('nvim-treesitter').install(language):await(function() treesitter_try_attach(buf, language) end)
+                    else
+                        -- try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
+                        treesitter_try_attach(buf, language)
                     end
                 end,
             })
@@ -143,7 +54,6 @@ return {
             move = {
                 enable = true,
                 set_jumps = true, -- whether to set jumps in the jumplist
-                -- LazyVim extention to create buffer-local keymaps
                 keys = {
                     goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
                     goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
