37ee6674f446faf0cf881b9b73f30ffebba7d223
Umut Sahin Onder <umut_sahin.oender@digits.schwarz>
2026-08-05 13:12:15 +0200

    add neotest-ginkgo for running ginkgo tests

--- lua/kickstart/plugins/test.lua
+++ lua/kickstart/plugins/test.lua
@@ -7,6 +7,7 @@ return {
         "antoinemadec/FixCursorHold.nvim",
         "nvim-treesitter/nvim-treesitter",
         -- Adapters
+        { "nvim-contrib/neotest-ginkgo" },
         {
             "fredrikaverpil/neotest-golang",
             version = "*", -- Optional, but recommended; track releases
@@ -39,11 +40,26 @@ return {
         { "[T",         function() require("neotest").jump.next({ status = 'failed' }) end, desc = "Failed Test Forward",                    ft = ft },
     },
     config = function()
+        -- Wrap neotest-golang to exclude Ginkgo test files (those importing onsi/ginkgo)
+        local golang_adapter = require("neotest-golang")({ runner = "gotestsum" })
+        local original_is_test_file = golang_adapter.is_test_file
+        golang_adapter.is_test_file = function(file_path)
+            if not original_is_test_file(file_path) then return false end
+            local f = io.open(file_path, "r")
+            if f then
+                local content = f:read(2048)
+                f:close()
+                if content:find('"github.com/onsi/ginkgo', 1, true) then
+                    return false
+                end
+            end
+            return true
+        end
+
         require("neotest").setup({
             adapters = {
-                require("neotest-golang")({
-                    runner = "gotestsum"
-                }),
+                require("neotest-ginkgo"),
+                golang_adapter,
             },
         })
     end,
